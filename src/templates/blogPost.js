32c8452810c1e0c64614409172a9bfb8ac7a129f
Nick <nicolai.voronin@gmail.com>
2018-11-04 18:18:32 +0200

    Build Gatsby page slugs dynamically

---
+++ src/templates/blogPost.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+const Temlate = props => {
+  return (
+    <div>
+      Blog Post Here
+    </div>
+  )
+}
+
+export default Temlate;
