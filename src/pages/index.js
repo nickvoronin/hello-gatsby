d6c8fa636ef8de68b41a0f887b6056fbed4aa8dc
Nick <nicolai.voronin@gmail.com>
2018-11-04 17:59:10 +0200

    Add GraphQL query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,52 @@
 import React from "react";
+import { graphql } from 'gatsby';
 import Header from '../components/Header'
 
-const Layout = () => {
+const Layout = ({ data }) => {
+  const { edges } = data.allMarkdownRemark;
+  console.log(edges)
   return (
     <div>
       <Header />
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        fontFamily: 'avenir'
+      }}>
+        {edges.map(edge => {
+          console.log(edge)
+          const { frontmatter } = edge.node;
+          return (
+            <div
+              key={frontmatter.path}
+              style={{marginBottom: '1rem'}}
+            >
+              {frontmatter.title}
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
+
+export const query = graphql`
+  query HomepageQuery {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date]}
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+            date
+          }
+        }
+      }
+    } 
+  }
+`
 
 export default Layout;
