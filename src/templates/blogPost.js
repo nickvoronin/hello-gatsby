f8045e394c6ddec7203bc7216dded86393ba7c44
Nick <nicolai.voronin@gmail.com>
2018-11-04 18:59:52 +0200

    Build a Blog Post Template with GraphQL

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-const Temlate = props => {
+const Temlate = ({ data }) => {
+  const { frontmatter: { title }, html } = data.markdownRemark;
   return (
-    <div>
-      Blog Post Here
-    </div>
+    <>
+      <h1>{title}</h1>
+      <div
+        className="blogpost"
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
+    </>
   )
 }
+
+export const query = graphql`
+  query($pathSlug: String!) {
+    markdownRemark(frontmatter: { path: { eq: $pathSlug } }) {
+      html,
+      frontmatter {
+        title
+        
+      }
+    }
+  }
+`;
 
 export default Temlate;
